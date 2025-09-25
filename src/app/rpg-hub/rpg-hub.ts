33ab11a161eea46b9cf85ef9de992790199f79cd
Vitor Hugo <vitorhugo-57@hotmail.com>
2025-09-25 19:22:04 -0300

    feat(rpg-hub):  tela de chat completa

--- src/app/rpg-hub/rpg-hub.ts
+++ src/app/rpg-hub/rpg-hub.ts
@@ -1,45 +1,70 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars, faBook, faPeopleGroup, faGear, faPaperPlane, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faBook,
+  faPeopleGroup,
+  faGear,
+  faPaperPlane,
+  faCaretDown,
+  faCaretUp,
+  faEllipsis,
+  faUserPlus,
+  faPlus
+} from '@fortawesome/free-solid-svg-icons';
 import { MatExpansionModule } from '@angular/material/expansion';
-
 
 @Component({
   selector: 'app-rpg-hub',
   imports: [FontAwesomeModule, MatExpansionModule],
   templateUrl: './rpg-hub.html',
-  styleUrl: './rpg-hub.scss'
+  styleUrl: './rpg-hub.scss',
 })
 export class RpgHub {
-  faBars = faBars
-  faBook = faBook
-  faPeopleGroup = faPeopleGroup
-  faGear = faGear
-  faPaperPlane = faPaperPlane
-  faCaretUp = faCaretUp
-  faCaretDown = faCaretDown
+  faBars = faBars;
+  faBook = faBook;
+  faPeopleGroup = faPeopleGroup;
+  faGear = faGear;
+  faPaperPlane = faPaperPlane;
+  faCaretUp = faCaretUp;
+  faCaretDown = faCaretDown;
+  faEllipsis = faEllipsis;
+  faUserPlus = faUserPlus;
+  faPlus = faPlus;
+
+  showSidebar = true;
+  toggleSidebar(event: Event) {
+    event.preventDefault();
+    this.showSidebar = !this.showSidebar;
+    if (!this.showSidebar){
+      this.showCharacters = false
+      this.showCampaigns = false
+    }
+  }
 
   items = [
-    { id: 1, title: 'Section 1' },
-    { id: 2, title: 'Section 2' },
-    { id: 3, title: 'Section 3' }
+    { id: 1, titulo: 'As minas perdidas de Phandelver', personagem: 1 },
+    { id: 2, titulo: 'A Maldição de Strahd', personagem: 3 },
+    { id: 3, titulo: 'O Templo do Mal Elemental', personagem: 2 },
   ];
 
   showCampaigns = false;
   toggleCampaigns(event: Event) {
     event.preventDefault();
+    this.showSidebar = true
     this.showCampaigns = !this.showCampaigns;
   }
+
+  characters = [
+    { id: 1, nome: 'Gorr', classe: 'Guerreiro', raca: 'Orc' },
+    { id: 2, nome: 'Athas', classe: 'Mago', raca: 'Elfo' },
+    { id: 3, nome: 'Jack', classe: 'Ladino', raca: 'Humano' },
+  ];
 
   showCharacters = false;
   toggleCharacters(event: Event) {
     event.preventDefault();
+    this.showSidebar = true
     this.showCharacters = !this.showCharacters;
-  }
-
-  showSidebar = false;
-  toggleSidebar(event: Event) {
-    event.preventDefault();
-    this.showSidebar = !this.showSidebar;
   }
 }
